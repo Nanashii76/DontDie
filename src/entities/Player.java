48b5005d349e0eccc945d0c61dc229c8ce4f354d
Paulo <pauloamino76@gmail.com>
2023-05-04 21:48:07 -0300

    adicionando os arquivos do  protótipo

---
+++ src/entities/Player.java
@@ -0,0 +1,116 @@
+package entities;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import main.Game;
+import world.Camera;
+import world.World;
+
+public class Player extends Entity {
+	
+	
+	public double life = 100;
+	
+	public boolean right,up,left,down;
+	public int right_dir = 0, left_dir = 1, idle_dir = 2;
+	public int dir = right_dir;
+	public int speed = 2;
+	
+	protected int frames1 = 0, maxFrames = 8, index = 0, maxIndex = 5;
+	protected boolean moved = false;
+	protected BufferedImage[] rightPlayer;
+	protected BufferedImage[] leftPlayer;
+	protected BufferedImage[] idlePlayer;
+	
+	public Player(int x, int y, int width, int height, BufferedImage sprite) {
+		super(x, y, width, height, sprite);
+		
+		rightPlayer = new BufferedImage[7];
+		leftPlayer = new BufferedImage[7];
+		idlePlayer = new BufferedImage[7];
+		
+		
+		for (int i = 0; i < rightPlayer.length; i++ ) {
+			rightPlayer[i] = Game.spritesheet.getSprite(64+(i*32), 0, 32, 32);
+		}
+		for (int i = 0; i < leftPlayer.length; i++ ) {
+			leftPlayer[i] = Game.spritesheet.getSprite(64+(i*32), 33, 32, 32);
+		}
+		for (int i = 0; i < idlePlayer.length; i++ ) {
+			idlePlayer[i] = Game.spritesheet.getSprite(64+(i*32), 65, 32, 32);
+		}
+		
+	}
+	
+	public void tick() {
+		
+		moved = false;
+		
+		if(right && World.isFree((int)(x+speed), y)) {
+			moved = true;
+			dir = right_dir;
+			x+=speed;
+		} else if (left && World.isFree((int)(x-speed), y)) {
+			moved = true;
+			dir = left_dir;
+			x-=speed;
+		}
+		
+		if(up && World.isFree(x, (int)(y-speed))) {
+			moved = true;
+			//dir = right_dir;
+			y-=speed;
+		} else if (down && World.isFree(x, (int)(y+speed))) {
+			moved = true;
+			//dir = right_dir;
+			y +=speed;
+		}
+		
+		if(moved) {
+			frames1++;
+			if(frames1 == maxFrames) {
+				frames1 = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			}
+		}
+		
+		if(!moved) {
+			dir = idle_dir;
+			frames1++;
+			if(frames1 == maxFrames) {
+				frames1 = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			}
+		}
+		
+		Camera.x = Camera.clamp((this.getX() - Game.WIDTH/2), 0, (World.WIDTH*32) - Game.WIDTH);
+		Camera.y = Camera.clamp((this.getY() - Game.HEIGHT/2), 0, (World.HEIGHT*32) - Game.HEIGHT);
+		
+				
+	}
+	
+	
+	public void render(Graphics g) {
+		
+		if (Game.character == 1 ) {
+			
+			if(dir == right_dir) {
+				g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			} else if (dir == left_dir) {
+				g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			} else if (dir == idle_dir) {
+				g.drawImage(idlePlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+			}
+	
+		}
+	}
+	
+	
+}
---
+++ src/main/Game.java
@@ -0,0 +1,254 @@
+package main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+import entities.Enemy;
+import entities.Entity;
+import entities.Player;
+import entities.Player2;
+import entities.Player3;
+import graficos.Spritesheet;
+import graficos.UI;
+import world.World;
+
+public class Game extends Canvas implements Runnable, KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	// Variables
+	public static int character = 1;
+	public static JFrame frame;
+	private boolean isRunning = true;
+	private Thread thread;
+	public static final int WIDTH = 260, HEIGHT = 160, SCALE = 3;
+
+	private BufferedImage image;
+
+	public static List<Entity> entities;
+	public static List<Enemy> enemies;
+	public static Spritesheet spritesheet;
+
+	public static Player player;
+	public static Player2 player2;
+	public static Player3 player3;
+	
+	public static World world;
+	
+	private Graphics g;
+
+	public static Random rand;
+	
+	public UI ui;
+	
+	// private Graphics2D g2;
+	/***/
+
+	// Construtor
+	public Game() {
+		
+		rand = new Random();
+		
+		// Para que os eventos de teclado funcionem
+		addKeyListener(this);
+
+		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		// Inicializando Objetos
+		ui = new UI();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		entities = new ArrayList<Entity>();
+		enemies = new ArrayList<Enemy>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		
+		player = new Player(0,0, 16, 16, spritesheet.getSprite(64, 0, 32, 32));
+		entities.add(Game.player);
+		//player2 = new Player2(20,0,16,16, spritesheet.getSprite(64, 97, 32, 32));
+		//player3 = new Player3(40,0,16,16, spritesheet.getSprite(64, 193, 32, 32));
+		//entities.add(player2);
+		//entities.add(player3);
+		world = new World("/map.png");
+	
+	}
+
+	// Criando a Janela
+	public void initFrame() {
+		frame = new JFrame("Dont Die");
+		frame.add(this);
+		frame.setResizable(false); //a tela não pode ser redimensionada
+		frame.pack();
+		frame.setLocationRelativeTo(null);// Janela inicializa no centro
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Fechar o programa por completo
+		frame.setVisible(true);// visibilidade da janela
+	}
+
+	// Threads
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+
+	public void tick() {
+		for (int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			if (e instanceof Player) {
+				// Ticks do Player
+			}
+			e.tick();
+		}
+	}
+
+	// O que vai ser mostrado em tela
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();// Sequencia de buffer para otimizar a renderizacao, lidando com
+														// performace grafica
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+
+		g = image.getGraphics();// Renderizar imagens na tela
+		g.setColor(new Color(0, 0, 0));
+		g.fillRect(0, 0, WIDTH * SCALE, HEIGHT * SCALE);
+
+		world.render(g);
+		
+		/* Render do jogo */
+		// g2 = (Graphics2D) g; //Transformei em um tipo g e foi feito um cast com o
+		// Graphics2D
+		for (int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+
+			e.render(g);
+		}
+
+		ui.render(g);
+		
+		/***/
+
+		g.dispose();// Limpar dados de imagem usados
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH * SCALE, HEIGHT * SCALE, null);
+		bs.show();
+	}
+
+	// Controle de FPS
+	public void run() {
+		// Variables
+		long lastTime = System.nanoTime();// Usa o tempo atual do computador em nano segundos, bem mais preciso
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;// Calculo exato de Ticks
+		double delta = 0;
+		requestFocus();
+		// Ruuner Game
+		while (isRunning == true) {
+			// System.out.println("My game is Running");
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			if (delta >= 1) {
+				tick();
+				render();
+				delta--;
+			}
+
+		}
+
+		stop(); // Garante que todas as Threads relacionadas ao computador foram terminadas,
+				// para garantir performance.
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// Esquerda e Direita
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+
+			player.right = true;
+			//player2.right = true;
+			//player3.right = true;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+
+			player.left = true;
+			//player2.left = true;
+			//player3.left = true;
+		}
+
+		// Cima e Baixo
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+
+			player.up = true;
+			//player2.up = true;
+			//player3.up = true;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = true;
+			//player2.down = true;
+			//player3.down = true;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// Esquerda e Direita
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+
+			player.right = false;
+			//player2.right = false;
+			//player3.right = false;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+			player.left = false;
+			//player2.left = false;
+			//player3.left = false;
+		}
+
+		// Cima e Baixo
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+
+			player.up = false;
+			//player2.up = false;
+			//player3.up = false;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			player.down = false;
+			//player2.down = false;
+			//player3.down = false;
+		}
+
+	}
+
+}
